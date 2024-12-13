fef5f66b0d8a189138a9760e34dd811806f5625b
codebuds-bash <dhruvsutar779@gmail.com>
2024-12-13 19:35:33 +0530

    updated register.html

--- backend/dbtest.js
+++ backend/dbtest.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://admin:9752748594@greenloop-cluster.cu5rv.mongodb.net/your_db_name?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://greenloop_new_user:9752748594@cluster0greenloop.cu5rv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0GREENLOOP';
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
