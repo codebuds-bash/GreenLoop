2417339b331c8e20e2c8c1ea5dfe9872e5cedbb4
Dhruv Suthar <dhruvsutar779@gmail.com>
2025-02-19 17:58:36 +0530

    updated some files

--- frontend/public/js/scripts.js
+++ frontend/public/js/scripts.js
@@ -1,13 +1,4 @@
 function toggleSidebar() {
-    let sidebar = document.querySelector(".sidebar");
-    let hamburger = document.querySelector(".hamburger");
-
-    sidebar.classList.toggle("open");
-
-    // Hide the hamburger when sidebar is open
-    if (sidebar.classList.contains("open")) {
-        hamburger.style.display = "none";
-    } else {
-        hamburger.style.display = "block";
-    }
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('open');
 }
